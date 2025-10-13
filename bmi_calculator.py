dfb17bc08b098e1b3e1bce9ca7834f8f657d087c
Kernel-Panic-Kill-Init <kernel.panic.kill.init@gmail.com>
2025-10-13 21:09:10 +0200

    Adding code to bmi_calculator.py

---
+++ bmi_calculator.py
@@ -0,0 +1,29 @@
+
+
+                                    # BMI Calculator
+                                    
+                                    
+                        #Validation for the inputs
+weight = input("Enter your weight in kg: ")
+height = input("Enter your height in cm: ")
+
+
+                        # Converting cm to m
+
+height_m = float(height) / 100
+
+                        # Calculating BMI according to its formula
+bmi = weight / (height_m ** 2)
+print(f"Your BMI is: {bmi: .2f} ")
+
+if bmi < 18.5:
+    print("Underweight")
+    
+elif 18.5 <= bmi < 25:
+    print("Normal weight")
+    
+elif 25 <= bmi < 30:
+    print("Overweight, try to exercise a bit!")
+    
+else 30 <= bmi:
+    print("Obese, take care of your health!")
