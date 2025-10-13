ba30c641eb71f03996b489a7556ffc4f7e906550
Kernel-Panic-Kill-Init <kernel.panic.kill.init@gmail.com>
2025-10-13 21:17:28 +0200

    Changing structure of the code, finding what doesn't work and adding a little bit of better looking syntax

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,29 +1,21 @@
+                                                # BMI Calculator
 
+                                                # Validation for the inputs
+weight = float(input("Enter your weight in kg: "))
+height = float(input("Enter your height in cm"))
 
-                                    # BMI Calculator
-                                    
-                                    
-                        #Validation for the inputs
-weight = input("Enter your weight in kg: ")
-height = input("Enter your height in cm: ")
+                                                # Converting cm to m
+height_m = height / 100
 
-
-                        # Converting cm to m
-
-height_m = float(height) / 100
-
-                        # Calculating BMI according to its formula
+                                                # Calculating BMI according to its formula
 bmi = weight / (height_m ** 2)
-print(f"Your BMI is: {bmi: .2f} ")
+print(f"Your BMI is: {bmi:.2f}")
 
 if bmi < 18.5:
     print("Underweight")
-    
 elif 18.5 <= bmi < 25:
     print("Normal weight")
-    
 elif 25 <= bmi < 30:
     print("Overweight, try to exercise a bit!")
-    
-else 30 <= bmi:
+else:
     print("Obese, take care of your health!")
