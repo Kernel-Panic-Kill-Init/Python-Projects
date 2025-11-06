a97563d1797f76a660daff3232b44ec9ca742602
Kernel-Panic-Kill-Init <kernel.panic.kill.init@gmail.com>
2025-11-06 23:39:40 +0100

    Making things work c:

--- dice_simulator/dice_simulator.py
+++ dice_simulator/dice_simulator.py
@@ -1,16 +1,25 @@
-############################################################################################ DICE SIMULATOR ############################################################################################
+import random    
 
-import random
+def menu():
+    while True:
+        dice = []
+        number_of_dices = int(input("Choose how many dices you want: "))
+        main_menu = int(input(
+            "Choose which dice you prefer:\n"
+            "1. 4 \n"
+            "2. 6 \n"
+            "3. 8 \n"
+            "4. 10 \n"
+        ))
 
+        dice_types = {1: 4, 2: 6, 3: 8, 4: 10}
+        if main_menu in dice_types:
+            sides = dice_types[main_menu]
+            for i in range(number_of_dices):
+                dice.append(random.randint(1, sides))
+            print(dice)
+            print(sum(dice))
+        else:
+            print("Invalid input. Try again!")
 
-dice = []
-number_of_dices = int(input("Choose how many dices you want: "))
-total = 0
-
-
-
-for i in range(number_of_dices):
-    dice.append(random.randint(1, 6))
-    
-    
-print(f"Rolls: {dice} \nTotal: {sum(dice)}")
+menu()
